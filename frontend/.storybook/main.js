df0b509e69fade586172edc76f7038bbcaf92215
monster <differentcodemonster@gmail.com>
2022-05-17 20:43:03 +0930

    feat: add cypress, add storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,12 +1,3 @@
-// module.exports = {
-// 	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-// 	addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-// 	framework: "@storybook/react",
-// 	core: {
-// 		builder: "@storybook/builder-webpack5",
-// 	},
-// }
-
 module.exports = {
 	stories: ["../src/stories/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
